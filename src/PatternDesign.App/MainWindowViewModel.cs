7546de4a8135d49e5939be29730c791b733f831b
Pieter van Geer <Pieter.vangeer@deltares.nl>
2018-09-09 11:46:44 +0200

    Initial sln setup

---
+++ src/PatternDesign.App/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+using System.Windows.Input;
+
+namespace PatternDesign.App
+{
+    public class MainWindowViewModel
+    {
+        public ICommand FileNewCommand
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public ICommand SaveProjectCommand
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public ICommand SaveProjectAsCommand
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public ICommand OpenProjectCommand
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
